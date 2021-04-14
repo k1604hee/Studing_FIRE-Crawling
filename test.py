6296eb312763d7d1d5058253be6400458ed4bae6
k1604hee <82390775+k1604hee@users.noreply.github.com>
2021-04-14 23:06:07 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-# Coding for Take Impormation In Naver Finance
-
 import datetime
 import pandas as pd
 import time
+import os
 
-def read_naver_toronbang():
-    start_time_all = time.time()
+# 진짜 이렇게 하면 Git 이 바뀌어?
+
+def read_naver_toronbang(code = '096040'):
+    start_time_all = time.time()  # 시작시간 가져오기
 
     #2021.02.27 goal now is get jongtobang all data
 
@@ -15,20 +16,19 @@ def read_naver_toronbang():
               - get page's date file on directory
     :return:
     """
-    code = '096040'
-    # code = '019550'
-    import os
-    year =  datetime.datetime.strftime(datetime.datetime.today(), '%Y')  # 년 가져오는 코드              #2021.02.21
+
+    year =  datetime.datetime.strftime(datetime.datetime.today(), '%Y')  # 월 가져오는 코드              #2021.02.21
     month = datetime.datetime.strftime(datetime.datetime.today(), '%m')  # 월 가져오는 코드              #2021.02.21
-    day = datetime.datetime.strftime(datetime.datetime.today(), '%d')  # 일 가져오는 코드              #2021.02.21
+    day = datetime.datetime.strftime(datetime.datetime.today(), '%d')  # 월 가져오는 코드              #2021.02.21
+
     pre_year = year
     pre_month = month
     pre_day = day
+
     path_month = f'C:/Users/user/PycharmProjects/project_FIRE/toronbang/{year}/{month}/{day}'  # 경로 만들기 코드        #2021.02.21
     path_code_df = f'{code}.csv'
-    path_df = os.path.join(path_month, path_code_df)
+    path_df = os.path.join(path_month, path_code_df) # code 경로
     # print(path_month)
-    path = 'jongtobang.csv'
 
     if os.path.exists(path_month):
         pass
@@ -37,9 +37,7 @@ def read_naver_toronbang():
         print('check')
 
 
-    pre_title = [] # before this title check that it has happen before
     import requests
-    start_date = None
     except_list = ['\xa0','\xa1','\xa2','\xa3','\xa4','\xa5','\xa6','\xa7','\xa8','\xa9','\xaa','\xab','\xac','\xad','\xae','\xaf',
                    '\xb0','\xb1','\xb2','\xb3','\xb4','\xb5','\xb6','\xb7','\xb8','\xb9','\xba','\xbb','\xbc','\xbd','\xbe','\xbf',
                    '\xc0','\xc1','\xc2','\xc3','\xc4','\xc5','\xc6','\xc7','\xc8','\xc9','\xca','\xcb','\xcc','\xcd','\xce','\xcf',
@@ -50,21 +48,19 @@ def read_naver_toronbang():
 
                    '\xe1','\xed','\xfd',
                    '\xe0','\xe1','\xe2','\xe3','\xe4','\xe5','\xe6','\xe7','\xe8','\xe9',
-                   '\u2000','\u2001','\u2002','\u2003','\u2004','\u2005','\u2006','\u2007','\u2008','\u2009',
+                   '\u2000','\u2001','\u2002','\u2003','\u2004','\u2005','\u2006','\u2007','\u2008','\u2009','\u200a','\u200b','\u200c','\u200d','\u200e',
                    '\u2010','\u2011','\u2012','\u2013','\u2014','\u2015','\u2016','\u2017','\u2018','\u2019',
                    '\u2022','\u2023','\u2024','\u2025','\u2026',
                    '\u0160','\u0161'
                    ]
-    # except_list =[]
     count = 0 # count 돌리기
-    # count = 646
+
     start = count
+
     while 1:
         start_time = time.time()
 
         # 수정 Test 진행
-
-        #
         year_list = []
         month_list = []
         day = []
@@ -93,25 +89,7 @@ def read_naver_toronbang():
         day_all = soap_main.find_all('span', class_='tah p10 gray03')  ###웹페이지 종토방 불러오기
         title_all = soap_main.find_all('td', class_='title')
 
-
-        # print(title_all)
-        # title_all = table_all.find(class_='title')
-
-        # href_all = title_all[0].find("a")["href"]
-        # print(href_all)
-        # print(url_main)
-        # print(soap_main)
-        # print(table_all)
-        # print(day_all)
-        # print(len(day_all))
-        # print(title_all[0].get_text())
-
-        # page 추가 시키면 됨
-
         url_main = f'https://finance.naver.com/item/board.nhn?code={code}&page={page}'
-
-        # print(len(title_all))
-        # print(len(day_all))
 
         # title 분리하는 방법
         for i in range(len(title_all)):
@@ -130,15 +108,6 @@ def read_naver_toronbang():
             source_deep = source_deep.replace('\r','___')
 
             # 내용 불러온거에 쓸데 없는 내용 제거
-            # source_deep = source_deep.replace('\xa0', '_xa0_')
-            # source_deep = source_deep.replace('\xa0', ' ') # 이게 맞는거 같아
-            # source_deep = source_deep.replace('\xa9', ' ') # 이게 맞는거 같아
-            # source_deep = source_deep.replace('\u2013', ' ') # 이게 맞는거 같아
-            # source_deep = source_deep.replace('\u2014', ' ') # 이게 맞는거 같아
-            # source_deep = source_deep.replace('\u2015', ' ') # 이게 맞는거 같아
-            # source_deep = source_deep.replace('\u2016 ', ' ')  # 이게 맞는거 같아
-            # source_deep = source_deep.replace('\xf3 ', ' ')  # 이게 맞는거 같아
-
             for list_i in except_list:
                 source_deep = source_deep.replace(list_i, ' ') # 이게 맞는거 같아
                 # print('replace', list_i,' :to empty space')
@@ -184,23 +153,135 @@ def read_naver_toronbang():
         # if slicing_month != pre_month:
 
 
+
+
+
+
+        ### 2021.04.12 수정 시작
+
+
         #여기서 날짜로 잘라
         if count == start:
             df = pd.DataFrame({'year': year_list,'month':month_list,'day': day,'time':time_list, 'title': title, 'source': source})
             pre_month = slicing_month
             pre_day = slicing_day
             pre_year = slicing_year
+            standard_day = df['day'][0]
+            print(standard_day)
+            for loof in range(len(df)):
+                if standard_day == df['day'][loof]:
+                    pass
+                else:
+                    year = df['year'][loof-1]
+                    month = df['month'][loof-1]
+                    day = df['day'][loof-1]
+                    path_month = f'C:/Users/user/PycharmProjects/project_FIRE/toronbang/{year}/{month}/{day}'  # 경로 만들기 코드        #2021.02.21
+
+                    if os.path.exists(path_month):
+                        pass
+                    else:
+                        os.makedirs(path_month)
+                        print('check')
+
+                    path_code_df = f'{code}.csv'
+                    path_df = os.path.join(path_month, path_code_df)
+
+                    pre_standard_day = standard_day
+                    standard_day = df['day'][loof]
+                    df_now = df[pre_standard_day == df['day']]
+                    df_now = df_now[['year','month','day','time','title','source']]
+                    df_now.to_csv(path_df, encoding='CP949', index=False)
+                    print(df_now)
+                    print(standard_day)
+                if loof == len(df)-1:
+                    if df['day'][0] ==  df['day'][loof]:
+                        pass
+                    else:
+                        year = df['year'][loof - 1]
+                        month = df['month'][loof - 1]
+                        day = df['day'][loof - 1]
+                        path_month = f'C:/Users/user/PycharmProjects/project_FIRE/toronbang/{year}/{month}/{day}'  # 경로 만들기 코드        #2021.02.21
+
+                        if os.path.exists(path_month):
+                            pass
+                        else:
+                            os.makedirs(path_month)
+                            print('check')
+
+                        path_code_df = f'{code}.csv'
+                        path_df = os.path.join(path_month, path_code_df)
+
+                        pre_standard_day = standard_day
+                        standard_day = df['day'][loof]
+                        df_now = df[pre_standard_day == df['day']]
+                        df_now = df_now[['year','month','day','time','title','source']]
+                        df_now.to_csv(path_df, encoding='CP949', index=False)
+                        print(df_now)
+                        print(standard_day)
+                        start = count +1
+
+
         else :
             _df = pd.DataFrame({'year': year_list,'month':month_list,'day': day,'time':time_list, 'title': title, 'source': source})
 
+            df = pd.concat([df, _df])
             if pre_day == slicing_day:
-                df = pd.concat([df,_df])
                 df.to_csv(path_df, encoding='CP949', index=False)
             else:
+                df = df.reset_index()
                 print(df)
-                df_now = df[pre_day == df['day']]
-                print(df_now)
-                df_now.to_csv(path_df, encoding='CP949', index=False)
+                print(len(df))
+                for loof in range(len(df)):
+                    if standard_day == df['day'][loof]:
+                        pass
+                    else:
+                        year = df['year'][loof - 1]
+                        month = df['month'][loof - 1]
+                        day = df['day'][loof - 1]
+                        path_month = f'C:/Users/user/PycharmProjects/project_FIRE/toronbang/{year}/{month}/{day}'  # 경로 만들기 코드        #2021.02.21
+
+                        if os.path.exists(path_month):
+                            pass
+                        else:
+                            os.makedirs(path_month)
+                            print('check')
+
+                        path_code_df = f'{code}.csv'
+                        path_df = os.path.join(path_month, path_code_df)
+
+                        pre_standard_day = standard_day
+                        standard_day = df['day'][loof]
+                        df_now = df[pre_standard_day == df['day']]
+                        df_now = df_now[['year','month','day','time','title','source']]
+                        df_now.to_csv(path_df, encoding='CP949', index=False)
+                        print(df_now)
+                        print(standard_day)
+                    if loof == len(df) - 1:
+                        if df['day'][0] == df['day'][loof]:
+                            pass
+                        else:
+                            year = df['year'][loof - 1]
+                            month = df['month'][loof - 1]
+                            day = df['day'][loof - 1]
+                            path_month = f'C:/Users/user/PycharmProjects/project_FIRE/toronbang/{year}/{month}/{day}'  # 경로 만들기 코드        #2021.02.21
+
+                            if os.path.exists(path_month):
+                                pass
+                            else:
+                                os.makedirs(path_month)
+                                print('check')
+
+                            path_code_df = f'{code}.csv'
+                            path_df = os.path.join(path_month, path_code_df)
+
+                            pre_standard_day = standard_day
+                            standard_day = df['day'][loof]
+                            df_now = df[pre_standard_day == df['day']]
+                            df_now = df_now[['year','month','day','time','title','source']]
+                            df_now.to_csv(path_df, encoding='CP949', index=False)
+                            print(df_now)
+                            print(standard_day)
+                            start = count+1
 
                 year = slicing_year
                 month = slicing_month
@@ -220,42 +301,19 @@ def read_naver_toronbang():
                 if os.path.exists(path_df):
                     df_last = pd.read_csv(path_df, encoding= 'CP949')
                     print(df_last)
-                    break
+                    # break
                 else:
                     pass
 
                 df = df[pre_day != df['day']]
                 pre_day=day
-                pre_month = month
-                pre_year = year
 
 
-
-
-            #
-            # path_month = f'C:/Users/user/PycharmProjects/project_FIRE/toronbang/{year}/{month}'  # 경로 만들기 코드        #2021.02.21
-            # path_code_df = f'{code}.csv'
-            # path_df = os.path.join(path_month, path_code_df)
-            #
-            # if os.path.exists(path_month):
-            #     pass
-            # else:
-            #     os.makedirs(path_month)
-            #     print('check')
-            #
-            # df.to_csv(path_df, encoding='CP949', index=False)
-            # df.to_csv(path_df, encoding='UTF-8', index=False)
-
-
-
-
-
-        pre_year = slicing_year
-        pre_month = slicing_month
         count = count + 1
         Run_Time = time.time() - start_time
         print(Run_Time)
 
+        ### 2021.04.12 수정 끝
 
     print(len(day))
     print(len(insight))
@@ -267,4 +325,4 @@ def read_naver_toronbang():
     Run_Time = time.time() - start_time_all
     print(Run_Time)
 
-read_naver_toronbang()
+read_naver_toronbang(code = '096040')
